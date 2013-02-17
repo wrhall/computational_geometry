992be61a021457a13d9ddd2abc26813e3974da36
Will Hall <wrsh07@gmail.com>
2013-02-16 22:17:32 -0800

    Create timing file
    benchmark two methods: find_interval and find_interval2
    
    Use this to pick the best one in the program.

---
+++ benchmark.rb
@@ -0,0 +1,12 @@
+# benchmark code
+# test ``find_interval`` and ``find_interval2``
+
+require "benchmark"
+require "./center"
+
+a = (0..10).to_a
+
+Benchmark.bm(7) do |x|
+  x.report("first:")  { (1..10000).each { a.find_interval  }
+  x.report("second:") { (1..10000).each { a.find_interval2 }
+end
