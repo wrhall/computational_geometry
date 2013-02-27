3932023416d3e3a1bcc3deaf9e8009c8ca732a6d
Will Hall <wrsh07@gmail.com>
2013-02-27 03:55:08 -0500

    Finished undoing my changes.

--- waiter_problem/benchmark.rb
+++ waiter_problem/benchmark.rb
@@ -22,12 +22,9 @@ e = rand_array(12, -100, 100)
 # end
 
 Benchmark.bm(7) do |x|
-  x.report("nine all:")	{1.times { b.find_best_fast } }
-  x.report("nine one:")	{1.times { b.find_a_best_fast } }
-  x.report("ten all:")	{1.times { c.find_best_fast } }
-  x.report("ten one:")	{1.times { c.find_a_best_fast } }
+  x.report("nine:")	{1.times { b.find_best_fast } }
+  x.report("ten:")	{1.times { c.find_best_fast } }
   x.report("eleven:")	{1.times { d.find_best_fast } }
-  x.report("ele one:")	{1.times { d.find_a_best_fast } }
 #   x.report("twelve:")	{1.times { e.find_best_fast } }
 end
 # b = (0..7).to_a
